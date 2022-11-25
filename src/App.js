8546b07fd613db37956e9e5ca49fb9630edfcd7e
Damian Kłeczek <damian.kleczek97@gmail.com>
2022-11-26 00:51:51 +0100

    Add title and searchbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import searchIcon from './search.svg';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="title">
+          <h1>Book API Search App</h1>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search a book..." />
+          <img src={searchIcon} alt="search icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
