da6771dd4a50c73e421f337255e5802f4de737e9
Damian Kłeczek <damian.kleczek97@gmail.com>
2022-12-01 00:11:06 +0100

    Implement search functionality in searchbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import './App.css';
+import { useState } from 'react';
+
+import BookCard from './components/BookCard';
 import Searchbar from './components/Searchbar';
 import Title from './components/Title';
 
+import './App.css';
+
 function App() {
+  const [books, setBooks] = useState(null);
+
+  const handleSetBooks = books => {
+    setBooks(books);
+  };
+
+  const BOOK_API_URL = 'https://www.googleapis.com/books/v1/volumes';
+
   return (
     <div className="app">
       <div className="container">
         <Title />
-        <Searchbar />
+        <Searchbar
+          searchUrl={BOOK_API_URL}
+          handleSetBooks={handleSetBooks}
+        />
+        <div className="books-container">
+          {books &&
+            books.items.map(book => {
+              return (
+                <BookCard
+                  key={book.id}
+                  book={book}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import searchIcon from '../search.svg';
 
-function Searchbar() {
+function Searchbar({ searchUrl, handleSetBooks }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = value => {
+    setSearchTerm(value);
+  };
+
+  const searchBooks = async () => {
+    const response = await fetch(`${searchUrl}?q=${searchTerm}`);
+    const data = await response.json();
+    handleSetBooks(data);
+    handleInputChange('');
+  };
+
   return (
     <div className="search">
-      <input type="text" placeholder="Search a book..." />
-      <img src={searchIcon} alt="search icon" />
+      <input
+        type="text"
+        placeholder="Search a book..."
+        value={searchTerm}
+        onChange={e => {
+          handleInputChange(e.target.value);
+        }}
+      />
+      <img
+        src={searchIcon}
+        alt="search icon"
+        onClick={() => {
+          searchBooks(searchTerm);
+        }}
+      />
     </div>
   );
 }
